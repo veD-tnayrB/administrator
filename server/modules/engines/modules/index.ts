6a16b16a7139b80a7f4c8f2564435ed1630dc68e
veD-tnayrB <Brayanmc.contacto@gmail.com>
2024-01-17 18:54:45 -0400

    feat: Temporally remove the list method

--- server/modules/engines/modules/index.ts
+++ server/modules/engines/modules/index.ts
@@ -7,49 +7,49 @@ export class ModulesManager extends Manager {
 		super({ model: DB.models.Modules });
 	}
 
-	list = async params => {
-		const modulesResponse = await this.model.findAll({});
-		const modules = modulesResponse.map(module => module.get({ plain: true }));
+	// list = async params => {
+	// 	const modulesResponse = await this.model.findAll({});
+	// 	const modules = modulesResponse.map(module => module.get({ plain: true }));
 
-		const permissionsResponse = await DB.models.ProfileModulePermissions.findAll({
-			include: [
-				{
-					model: DB.models.Profiles,
-					as: 'profile',
-				},
-				{
-					model: DB.models.Permissions,
-					as: 'permission',
-				},
-			],
-		});
-		const permissions = permissionsResponse.map(permission => permission.get({ plain: true }));
+	// 	const permissionsResponse = await DB.models.ProfileModulePermissions.findAll({
+	// 		include: [
+	// 			{
+	// 				model: DB.models.Profiles,
+	// 				as: 'profile',
+	// 			},
+	// 			{
+	// 				model: DB.models.Permissions,
+	// 				as: 'permission',
+	// 			},
+	// 		],
+	// 	});
+	// 	const permissions = permissionsResponse.map(permission => permission.get({ plain: true }));
 
-		const modulesPermissions = new Map();
-		permissions.forEach(permission => {
-			const existingPermissions = modulesPermissions.get(permission.moduleId) || [];
-			existingPermissions.push({
-				permissionId: permission.permission.id,
-				permissionName: permission.permission.name,
-				profileId: permission.profileId,
-				profileName: permission.profile.name,
-			});
-			modulesPermissions.set(permission.moduleId, existingPermissions);
-		});
+	// 	const modulesPermissions = new Map();
+	// 	permissions.forEach(permission => {
+	// 		const existingPermissions = modulesPermissions.get(permission.moduleId) || [];
+	// 		existingPermissions.push({
+	// 			permissionId: permission.permission.id,
+	// 			permissionName: permission.permission.name,
+	// 			profileId: permission.profileId,
+	// 			profileName: permission.profile.name,
+	// 		});
+	// 		modulesPermissions.set(permission.moduleId, existingPermissions);
+	// 	});
 
-		console.log('PER,ONS => ', modulesPermissions);
+	// 	console.log('PER,ONS => ', modulesPermissions);
 
-		const result = modules.map(module => {
-			return {
-				...module,
-				permissions: modulesPermissions.get(module.id) || [],
-			};
-		});
+	// 	const result = modules.map(module => {
+	// 		return {
+	// 			...module,
+	// 			permissions: modulesPermissions.get(module.id) || [],
+	// 		};
+	// 	});
 
-		console.log('RESULT => ', result);
+	// 	console.log('RESULT => ', result);
 
-		return { status: true, data: result };
-	};
+	// 	return { status: true, data: result };
+	// };
 }
 
 export /*bundle*/ const Modules = new ModulesManager();
