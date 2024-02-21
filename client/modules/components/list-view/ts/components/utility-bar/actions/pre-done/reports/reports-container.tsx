e8164069334e5c884f4307310270fd52d2d525e4
veD-tnayrB <Brayanmc.contacto@gmail.com>
2024-02-21 07:54:24 -0400

    fix: its being implemented the generate report buttons

--- client/modules/components/list-view/ts/components/utility-bar/actions/pre-done/reports/reports-container.tsx
+++ client/modules/components/list-view/ts/components/utility-bar/actions/pre-done/reports/reports-container.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { IActions } from '../../actions';
-import { GenerateExcel } from './generate-excel';
 import { v4 as uuid } from 'uuid';
 import { GenerateCSV } from './generate-csv';
 import { Import } from './import';
+import { GenerateReports } from './generate-reports';
 
 export const ReportsContainer = (props: IActions) => {
 	if (!props.generateReport) return;
 	const actions = [];
 
+	const generateAllReports = props.generateReport?.excel && props.generateReport?.csv;
 	if (props.generateReport?.excel) actions.push(<GenerateExcel {...props.generateReport.excel} />);
+	if (generateAllReports) actions.push(<GenerateReports />);
 	if (props.generateReport?.csv) actions.push(<GenerateCSV {...props.generateReport.excel} />);
 	if (props.import) actions.push(<Import {...props.import} />);
 
